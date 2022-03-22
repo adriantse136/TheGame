7465b29d670810c4bf6f617a876af5d0a45e4af1
adriantse136 <54153247+adriantse136@users.noreply.github.com>
2022-03-22 14:52:00 -0500

    PLEASE WORK

---
+++ TheGame/Assets/Skrips/movement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float MovementSpeed = 1;
+    Animator animate;
+    private Rigidbody2D rigid;
+    // Start is called before the first frame update  
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame  
+    void Update()
+    {
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement * 100, 0, 0) * Time.deltaTime * MovementSpeed;
+        //print("update");
+
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(rigid.velocity.y) < 0.001f)
+        {
+            this.GetComponent<AudioSource>().Play();
+            rigid.AddForce(new Vector2(0, 4), ForceMode2D.Impulse);
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
+        {
+            //animate.SetInteger("state", 1);
+            //print("right");
+        }
+        else
+        {
+            //animate.SetInteger("state", 0);
+        }
+    }
+}
