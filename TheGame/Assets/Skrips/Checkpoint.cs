834df1e3afebd9068998a7fafcfb9d02c897c9c3
adriantse136 <54153247+adriantse136@users.noreply.github.com>
2022-04-06 15:10:14 -0500

    how do i get it to work

--- TheGame/Assets/Skrips/Checkpoint.cs
+++ TheGame/Assets/Skrips/Checkpoint.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
+    public string Next = "";
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        LevelManager.NextLevel;
+
     }
 }
--- TheGame/Assets/Skrips/LevelManager.cs
+++ TheGame/Assets/Skrips/LevelManager.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    
+    public void NextLevel(string NextSceneName)
+    {
+        SceneManager.LoadScene(NextSceneName);
+    }    
 }
